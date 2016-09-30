6ba4af8c39246f848f8c6eb2abd023d45dbd727b
Phil J. Laszkowicz <phil@fillip.pro>
2016-09-30 23:02:12 +0300

    Added Package.swift and auxilliary resources to the project.

---
+++ Package.swift
@@ -0,0 +1,16 @@
+
+import PackageDescription
+
+let package = Package(
+    name: "Euclid",
+    targets: [
+        Target(
+            name: "Euclid",
+            dependencies: []
+        )
+    ],
+    dependencies: [
+
+    ],
+    exclude: ["Makefile"]
+)
